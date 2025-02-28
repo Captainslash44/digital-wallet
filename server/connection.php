8dd7b3d23c22c221161319db24102d5a854f219d
CaptainSlash44 <michaelnjeim44@gmail.com>
2025-02-28 13:55:04 +0200

    created api files, without code

---
+++ server/connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$host= "localhost";
+$user = "root";
+$password = "";
+$db_name = "digital-wallet";
+
+$conn = new mysqli($host, $user, $password, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
